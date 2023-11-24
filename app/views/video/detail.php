371eb3349c65386645d196c3d19e178706a5dcb9
DESKTOP-O2KNQPR\DFL <fanwenxiang326@163.com>
2023-11-24 19:55:07 +0800

    修改

--- app/views/video/detail.php
+++ app/views/video/detail.php
@@ -2,7 +2,4 @@
 <?php echo $item['title'] ?>
 </div>
 
-<video style="max-width: 100vw; max-height: 100vh;" src="<?php echo $item['video_url'] ?>"></video>
-
-
-<a class="big" href="/item/index">返回</a>
+<video controls style="max-width: 100vw; max-height: 100vh;" src="<?php echo $item['video_url'] ?>"></video>
