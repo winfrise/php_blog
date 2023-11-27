c5d89b6cce86857955153841e1cb484f2c279b50
DESKTOP-O2KNQPR\DFL <fanwenxiang326@163.com>
2023-11-27 14:29:39 +0800

    添加author-card

---
+++ app/views/author-card.php
@@ -0,0 +1,95 @@
+<style>
+.author-card {
+  background: #fff none repeat scroll 0 0;
+  border-radius: 4px;
+  padding-bottom: 15px;
+}
+.author-card .author-head {
+  background-image: url("../img/author-head.jpeg");
+  background-position: 0 50%;
+  background-size: 100% auto;
+  border-bottom: 1px solid #e1e8ed;
+  border-radius: 4px 4px 0 0;
+  height: 100px;
+  width: 100%;
+}
+.author-card .author-avatar {
+  text-align: center;
+}
+.author-card .author-avatar > a {
+  background-color: #fff;
+  border-radius: 50%;
+  display: inline-block;
+  margin: -40px 5px 0 0;
+  max-width: 100%;
+  padding: 1px;
+  vertical-align: bottom;
+}
+.author-card .author-avatar > a > img {
+  border: 2px solid #fff;
+  border-radius: 50%;
+  color: #fff;
+  height: 80px;
+  width: 80px;
+}
+.author-card .author-basic {
+  clear: both;
+  text-align: center;
+  width: 100%;
+  margin: 10px 0 10px 0;
+}
+.author-card .author-basic .author-nickname {
+  margin-bottom: 5px;
+}
+.author-card .author-basic .author-nickname > a {
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 21px;
+}
+.author-card .author-statistics {
+    display: flex;
+  padding: 5px 0;
+  text-align: center;
+  margin: 0;
+}
+.author-card .author-statistics .statistics-text {
+  color: #777;
+  margin-bottom: 5px;
+}
+.author-card .author-statistics .statistics-nums {
+  font-size: 16px;
+  font-weight: 600;
+}
+</style>
+
+<div class="author-card">
+    <div class="author-head" style="background-image:url('/assets/addons/cms/img/author-head.jpeg');"></div>
+
+    <div class="author-avatar">
+        <a href="/u/8">
+            <img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZlcnNpb249IjEuMSIgaGVpZ2h0PSIxMDAiIHdpZHRoPSIxMDAiPjxyZWN0IGZpbGw9InJnYigxNjAsMTc5LDIyOSkiIHg9IjAiIHk9IjAiIHdpZHRoPSIxMDAiIGhlaWdodD0iMTAwIj48L3JlY3Q+PHRleHQgeD0iNTAiIHk9IjUwIiBmb250LXNpemU9IjUwIiB0ZXh0LWNvcHk9ImZhc3QiIGZpbGw9IiNmZmZmZmYiIHRleHQtYW5jaG9yPSJtaWRkbGUiIHRleHQtcmlnaHRzPSJhZG1pbiIgZG9taW5hbnQtYmFzZWxpbmU9ImNlbnRyYWwiPumAojwvdGV4dD48L3N2Zz4=">
+        </a>
+    </div>
+
+    <div class="author-basic">
+        <div class="author-nickname">
+            <a href="/u/8">逢赞怡</a>
+        </div>
+        <span class="text-muted">这家伙很懒，什么也没写！</span>
+    </div>
+
+    <div class="row author-statistics">
+        <div class="layui-col-xs4">
+            <div class="statistics-text">文章</div>
+            <div class="statistics-nums"><a href="/u/8/archives">3</a></div>
+        </div>
+        <div class="layui-col-xs4">
+            <div class="statistics-text">评论</div>
+            <div class="statistics-nums"><a href="/u/8/comment">2665</a></div>
+        </div>
+        <div class="layui-col-xs4">
+            <div class="statistics-text">加入时间</div>
+            <div class="statistics-nums">3年前</div>
+        </div>
+    </div>
+</div>
--- app/views/video/detail.php
+++ app/views/video/detail.php
@@ -1,5 +1,29 @@
-<div>
-<?php echo $item['title'] ?>
-</div>
+<style>
+.wrapper {
+    width: 1200px;
+    margin: 0 auto;
+}
+.title {
+    font-size: 16px;
+    font-weight: bold;
+    color: #666;
+    line-height: 30px;
+}
+.video {
+    width: 780px;
+    height: 440px;
+}
+</style>
 
-<video controls style="max-width: 100vw; max-height: 100vh;" src="<?php echo $item['video_url'] ?>"></video>
+<div class="wrapper">
+    <main class="layui-col-md8">
+        <div class="title">
+            <?php echo $item['title'] ?>
+        </div>
+
+        <video class="video" controls src="<?php echo $item['video_url'] ?>"></video>
+    </main>
+    <aside class="layui-col-md4">
+      <?php include './app/views/author-card.php' ?>
+    </aside>
+</div>
