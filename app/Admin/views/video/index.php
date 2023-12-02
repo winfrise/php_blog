63f36a898d503372455aeee704725a14f2a7b8d9
DESKTOP-O2KNQPR\DFL <fanwenxiang326@163.com>
2023-12-02 15:56:26 +0800

    修改

--- app/Admin/views/video/index.php
+++ app/Admin/views/video/index.php
@@ -4,7 +4,46 @@
     <a href="/admin/video/detail?action=add" class="layui-btn layui-btn-primary">新建</a>
 </form>
 
-<p></p>
+<table class="layui-hide" id="test"></table>
+
+
+<script>
+layui.use('table', function(){
+  var table = layui.table;
+
+  table.render({
+    elem: '#test',
+    url: '/api/video/list',
+    limit: 20,
+    parseData: function(res){ //res 即为原始返回的数据
+      return {
+        "code": res.code, //解析接口状态
+        "msg": res.message, //解析提示文本
+        "count": res.data.total, //解析数据长度
+        "data": res.data.list //解析数据列表
+      };
+    },
+    request: {
+      pageName: 'page_num' //页码的参数名称，默认：page
+      ,limitName: 'page_size' //每页数据量的参数名，默认：limit
+    },
+    page: true,
+    cellMinWidth: 80, //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+    cols: [[
+      {field:'id', width:80, title: 'ID', sort: true}
+      ,{field:'title', title: '视频标题'}
+      ,{field:'video_url', title: '视频地址'}
+      ,{field:'video_poster', title: '视频封面'}
+      ,{field:'content', title: '内容'}
+      ,{field:'suffix', width:60, title: '后缀'}
+      ,{field:'create_time', title: '创建时间'}
+      ,{field:'updated_time', title: '更新时间'}
+    ]]
+  });
+});
+</script>
+
+
 
 <table class="layui-table">
     <tr>
